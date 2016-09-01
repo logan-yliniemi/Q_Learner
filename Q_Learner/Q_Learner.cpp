e6969f1e9294ed6bfb7ca34a456a378cd54d8b94
Logan Yliniemi <loganyliniemi@Logans-MacBook-Pro.local>
2016-09-01 12:38:42 -0700

    File Structure
    Initial file structure.

---
+++ Q_Learner/Q_Learner.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Q_Learner
+//
+//  Created by Logan Yliniemi on 9/1/16.
+//  Copyright © 2016 Logan Yliniemi. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "begin program" << endl;
+    
+    cout << endl << "end program" << endl;
+    return 0;
+}
